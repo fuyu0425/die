698612c5339450d709d13423b29956307455c239
Yu-Fu Fu <yufu@yfu.tw>
2025-03-04 05:08:32 -0500

    revert-pdf.sh: check emcas is installed first

--- bin/revert-pdf.sh
+++ bin/revert-pdf.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+# revert pdf after building
+# skipping and exit gracefully if emacs is not installed
+
+
+# Check if emacsclient is installed
+if ! command -v emacsclient &> /dev/null; then
+    # echo "Error: emacsclient is not installed. Please install Emacs."
+    exit 0
+fi
 
 # Check if a file path is provided as an argument
 if [ -z "$1" ]; then
     echo "Usage: $0 path/to/your/file.pdf"
-    exit 1
+    exit 0
 fi
 
 # Resolve the absolute path of the PDF file
@@ -11,14 +20,14 @@ PDF_PATH=$(realpath "$1")
 
 # Check if the file exists
 if [ ! -f "$PDF_PATH" ]; then
-    # echo "Error: File '$PDF_PATH' not found."
-    exit 1
+    echo "Error: File '$PDF_PATH' not found."
+    exit 0
 fi
 
 # Check if Emacs server is running
 if ! emacsclient -q -a false --eval "nil" &>/dev/null; then
     # echo "Error: Emacs server is not running. Please start Emacs with --daemon or ensure the server is running."
-    exit 1
+    exit 0
 fi
 
 # Command to revert the PDF buffer in Emacs with error handling
